f2b3c1bb55386f829e8cf6a1860321025467b634
phil <phil@ratehub.ca>
2017-07-06 15:29:06 -0400

    Add initial implementation

---
+++ index.js
@@ -0,0 +1,60 @@
+/**
+ * Copyright Facebook, ratehub.
+ * All rights reserved.
+ *
+ * This code is intended to closely match the behaviour of checkPropTypes() from
+ * facebook/prop-types. The license for that code can be found here:
+ * https://github.com/facebook/prop-types/blob/be165febc8133dfbe2c45133db6d25664dd68ad8/LICENSE
+ *
+ * That function's source:
+ * https://github.com/facebook/prop-types/blob/be165febc8133dfbe2c45133db6d25664dd68ad8/checkPropTypes.js
+ */
+
+/**
+ * Check if the values match with the type specs
+ * Return a type error message or null
+ *
+ * @param {object} typeSpecs Map of name to a ReactPropType
+ * @param {object} values Runtime values that need to be type-checked
+ * @param {string} location e.g. "prop", "context", "child context"
+ * @param {string} componentName Name of the component for error messages.
+ * @param {?Function} getStack Returns the component stack.
+ */
+function checkPropTypes(typeSpecs, values, location, componentName, getStack) {
+  if (process.env.NODE_ENV !== 'production') {
+    const ReactPropTypesSecret = require('prop-types/lib/ReactPropTypesSecret');
+    var name = componentName || 'React class';
+    for (var typeSpecName in typeSpecs) {
+      if (typeSpecs.hasOwnProperty(typeSpecName)) {
+        var error;
+        if (typeof typeSpecs[typeSpecName] !== 'function') {
+          return (name + ': ' + location + ' type `' + typeSpecName + '` is ' +
+            'invalid; it must be a function, usually from React.PropTypes.');
+        } else {
+          // Prop type validation may throw. In case they do, catch and save the
+          // exception as the error.
+          try {
+            error = typeSpecs[typeSpecName](values, typeSpecName, componentName,
+              location, null, ReactPropTypesSecret);
+          } catch (ex) {
+            error = ex;
+          }
+        }
+        if (error && !(error instanceof Error)) {
+          return (name + ': type specification of ' + location + ' `' +
+            typeSpecName + '` is invalid; the type checker function must ' +
+            'return `null` or an `Error` but returned a ' + typeof error + '. '+
+            'You may have forgotten to pass an argument to the type checker ' +
+            'creator (arrayOf, instanceOf, objectOf, oneOf, oneOfType, and ' +
+            'shape all require an argument).');
+        }
+        if (error instanceof Error) {
+          var stack = getStack && getStack() || '';
+          return 'Failed ' + location + ' type: ' + error.message + stack;
+        }
+      }
+    }
+  }
+}
+
+module.exports = checkPropTypes;
---
+++ test.js
@@ -0,0 +1,41 @@
+const test = require('tape');
+const PropTypes = require('prop-types');
+const checkPropTypes = require('.');
+
+test('Good props check falsy', function(assert) {
+  assert.plan(2);
+  assert.ifError(checkPropTypes({}, {}, 'prop'));
+  assert.ifError(checkPropTypes({x: PropTypes.number}, {x: 1}, 'prop'));
+});
+
+test('Bad props return a message', function(assert) {
+  assert.plan(1);
+  var message = checkPropTypes({x: PropTypes.number}, {x: ''}, 'prop', 'C');
+  assert.equal(message, 'Failed prop type: Invalid prop `x` of type `string` supplied to `C`, expected `number`.');
+});
+
+test('Bad propTypes fail check', function(assert) {
+  assert.plan(2);
+  var message;
+
+  message = checkPropTypes({x: null}, {}, 'prop', 'C');
+  assert.equal(message, 'C: prop type `x` is invalid; it must be a function, usually from React.PropTypes.');
+
+  message = checkPropTypes({x: function() {return 1}}, {}, 'prop', 'C');
+  assert.equal(message, 'C: type specification of prop `x` is invalid; the type checker function must return `null` or an `Error` but returned a number. You may have forgotten to pass an argument to the type checker creator (arrayOf, instanceOf, objectOf, oneOf, oneOfType, and shape all require an argument).');
+});
+
+test('Throwing propTypes fail check', function(assert) {
+  assert.plan(1);
+  function throwingType() {
+    throw new Error('sup');
+  }
+  var message = checkPropTypes({x: throwingType}, {}, 'prop', 'C');
+  assert.equal(message, 'Failed prop type: sup');
+});
+
+test('Does not avoid failing the same problem multiple times', function(assert) {
+  assert.plan(2);
+  assert.true(checkPropTypes({x: PropTypes.string}, {x: 1}, 'prop'));
+  assert.true(checkPropTypes({x: PropTypes.string}, {x: 1}, 'prop'));
+});
